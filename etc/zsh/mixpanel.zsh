2d175242e1cd1c6e9426d45dbfbb86db1ede7035
Rohith Ravi <rohith.ravi@mixpanel.com>
2015-12-21 00:09:07 +0000

    vim

--- etc/zsh/mixpanel.zsh
+++ etc/zsh/mixpanel.zsh
@@ -6,3 +6,6 @@
 # export MP_ENV_TYPE=dev
 
 export VAULT_TOKEN=58d07566-6d4a-9a73-bb25-54476fa0278e
+
+export SL_USERNAME='rohith.ravi'
+export SL_API_KEY=f4836531d385dbdf0c2cf67b912d4c28225725606b73082c28468bf99d6a38ea
