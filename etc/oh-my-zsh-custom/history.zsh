c3cb419fdfbe587a715b9dc965be168d4d4f43ce
Rohith Ravi <rohith.ravi@mixpanel.com>
2016-01-31 22:35:56 +0000

    adding history to new oh-my-zsh

---
+++ etc/oh-my-zsh-custom/history.zsh
@@ -0,0 +1,10 @@
+HISTFILE=~/.history
+HISTSIZE=5000
+SAVEHIST=$HISTSIZE
+
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+# this causes history corruption as of zsh 5.0.2 (x86_64-pc-linux-gnu)
+# setopt HIST_REDUCE_BLANKS
+setopt SHARE_HISTORY
+setopt EXTENDED_HISTORY
