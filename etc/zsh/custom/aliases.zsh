ef44c3585e19f27afdc8595a4628ac5b81bbd8d4
Rohith Ravi <rohith.ravi@mixpanel.com>
2016-10-08 21:32:20 +0000

    cleaning up zsh

--- etc/zsh/custom/aliases.zsh
+++ etc/zsh/custom/aliases.zsh
@@ -32,7 +32,6 @@ function gcb {
 
 # kubernetes
 alias k='kubectl'
-alias kpods='kubectl get pods'
 alias dpod='kubectl describe pod'
 alias dsvc='kubectl describe service'
 alias ddep='kubectl describe deployment'
@@ -46,4 +45,47 @@ function kshell {
     else
         kubectl exec $pod -t -i -- env COLUMNS=$(tput cols) LINES=$(tput lines) TERM=$TERM /bin/bash
     fi
+}
+
+function kpods {
+    local grepStr=$1
+
+    if [ -n "$grepStr" ]; then
+        kubectl get pods | grep "$grepStr"
+    else
+        kubectl get pods
+    fi
+}
+
+function kroll {
+    local role=$1
+    pod_names=$(kubectl get pods --selector="role=${role}" -o=jsonpath="{.items[*].metadata.name}")
+
+    if [ -z "${pod_names}" ]; then
+        echo "no pods found. exiting"
+        return
+    fi
+
+    echo -en "About to delete $(wc -w <<< "${pod_names}") pods: ${pod_names}\nContinue (y/N): "
+    read -q
+
+    if [ $? -ne 0 ]; then
+        echo -e "\naborting"
+        return
+    fi
+
+    # ${=foo} enables splitting by IFS, aka by word
+    for p in "${=pod_names}"; do
+        echo
+        cmd="kubectl delete pod ${p}"
+        echo $cmd
+        /bin/bash -c "${cmd}"
+
+        count=5
+        while [ ${count} -gt 0 ]; do
+            ((count-=1))
+            echo -en "\rwaiting... ${count}"
+            sleep 1
+        done
+    done
 }
