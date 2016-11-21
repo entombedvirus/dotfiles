f7f90f31c073b77d2a228383e57f185439a7a75b
Rohith Ravi <rohith.ravi@mixpanel.com>
2016-11-21 19:46:34 +0000

    vim + history + aliases

--- etc/zsh/custom/aliases.zsh
+++ etc/zsh/custom/aliases.zsh
@@ -14,20 +14,36 @@ alias gdn="git diff --name-only"
 # oh-my-zsh git plugin defines an alias that inteferes with this function
 unalias gcb 2>/dev/null
 function gcb {
-  anyframe-widget-checkout-git-branch
-  # branch=$1
-  # count=`gb | grep $1  | wc -l`
-  # if [[ $count -ge 2 ]]; then
-  #   echo "too many branches with $branch in the name:"
-  #   gb | grep $1
-  #   return
-  # fi
+  branch=$1
+  if [[ -z "$branch" ]]; then
+    anyframe-widget-checkout-git-branch
+    return
+  fi
 
-  # if [[ $count -eq 1 ]]; then
-  #   git co `gb | grep $1`
-  # else
-  #   echo "no branches with $branch in the name"
-  # fi
+  count=`gb | grep $1  | wc -l`
+  if [[ $count -ge 2 ]]; then
+    anyframe-source-git-branch -n \
+      | anyframe-selector-auto "$branch" \
+      | awk '{print $1}' \
+      | anyframe-action-execute git checkout
+    return
+  fi
+
+  if [[ $count -eq 1 ]]; then
+    git co `gb | grep $1`
+  else
+    echo "no branches with $branch in the name"
+  fi
+}
+
+function kctx {
+    name=$1
+    kubectl config get-contexts \
+      | awk '$5==""{print $2 "\tdefault"}; $5!=""{print $2 "\t" $5}' \
+      | xargs -L2  printf "%-60s %-30s\n" \
+      | fzf --query "$name" --header-lines=1 \
+      | awk '{print $1}' \
+      | anyframe-action-execute kubectl config use-context
 }
 
 # kubernetes
@@ -59,33 +75,33 @@ function kpods {
 
 function kroll {
     local role=$1
+    local delay=${2:-5}
     pod_names=$(kubectl get pods --selector="role=${role}" -o=jsonpath="{.items[*].metadata.name}")
-
-    if [ -z "${pod_names}" ]; then
+    if [ -z "${pod_names}" ]
+    then
         echo "no pods found. exiting"
         return
     fi
-
     echo -en "About to delete $(wc -w <<< "${pod_names}") pods: ${pod_names}\nContinue (y/N): "
     read -q
-
-    if [ $? -ne 0 ]; then
+    if [ $? -ne 0 ]
+    then
         echo -e "\naborting"
         return
     fi
-
-    # ${=foo} enables splitting by IFS, aka by word
-    for p in "${=pod_names}"; do
+    for p in "${=pod_names}"
+    do
         echo
         cmd="kubectl delete pod ${p}"
         echo $cmd
         /bin/bash -c "${cmd}"
-
-        count=5
-        while [ ${count} -gt 0 ]; do
+        count=${delay}
+        while [ ${count} -gt 0 ]
+        do
             ((count-=1))
             echo -en "\rwaiting... ${count}"
             sleep 1
         done
     done
+    kubectl get pods --watch --selector="role=${role}"
 }
