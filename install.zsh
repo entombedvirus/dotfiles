bb7a17751b6e2c60cc03c58e21b2af467de7ec48
Rohith Ravi <rohith.ravi@mixpanel.com>
2017-06-06 10:52:11 -0700

    adding script to install packages

---
+++ install.zsh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+# vim: set ft=zsh
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ ! -d ${HOME}/.antigen ]; then
+    git clone git@github.com:zsh-users/antigen.git ${HOME}/.antigen
+fi
+
+if ! command -v brew >/dev/null 2>&1; then
+    # Install linuxbrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+fi
+
+brew install go fzf ctags-exuberant autossh the_silver_searcher
+
+export GOPATH=${HOME}/work/go
+mkdir -p ${GOPATH}
+
+LEMONADE_PATH=${GOPATH}/bin/lemonade
+if [ ! -e "$LEMONADE_PATH" ]; then
+    go get -d github.com/pocke/lemonade
+    make -C $GOPATH/src/github.com/pocke/lemonade install
+fi
