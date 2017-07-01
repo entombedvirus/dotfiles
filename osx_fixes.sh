34564d102de9b040d7e07cb94e4cb49bb14095d1
Rohith Ravi <rohith.ravi@mixpanel.com>
2017-07-01 11:04:42 -0700

    gvimrc consolidation

---
+++ osx_fixes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# reload Dock
+killall Dock
