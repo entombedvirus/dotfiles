2078532a0eff8bd0f706d3b8e3338e9c465d7843
Rohith Ravi <rohith.ravi@mixpanel.com>
2017-07-01 15:41:01 -0700

    devt cleanup + osx_fixes.sh

--- bin/osx_fixes.sh
+++ bin/osx_fixes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 # reload Dock
