db20253097afbfb8feb63d2555cd669aa8a6db0e
Rohith Ravi <rohith.ravi@mixpanel.com>
2018-08-07 11:00:04 -0700

    gcpdevbox: initial setup

--- install.zsh
+++ install.zsh
@@ -6,27 +6,28 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# install linux brew deps
+sudo apt-get install build-essential curl file git
+
+if ! command -v brew >/dev/null 2>&1; then
+    # Install linuxbrew
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+fi
+
+brew install fzf ctags-exuberant global the_silver_searcher
+
+# source code tagging
+# brew tap universal-ctags/universal-ctags
+# brew install --HEAD universal-ctags
+# brew install global
+
 if [ ! -d ${HOME}/.antigen ]; then
     git clone git@github.com:zsh-users/antigen.git ${HOME}/.antigen
 fi
 
-if ! command -v brew >/dev/null 2>&1; then
-    # Install linuxbrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-fi
-
-brew install go fzf ctags-exuberant autossh the_silver_searcher
-
-# source code tagging
-brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
-brew install global
-
-export GOPATH=${HOME}/work/go
-mkdir -p ${GOPATH}
-
-LEMONADE_PATH=${GOPATH}/bin/lemonade
+LEMONADE_PATH=${HOME}/.bin/lemonade
 if [ ! -e "$LEMONADE_PATH" ]; then
-    go get -d github.com/pocke/lemonade
-    make -C $GOPATH/src/github.com/pocke/lemonade install
+    mkdir -p $(dirname "$LEMONADE_PATH")
+    curl --silent -L https://github.com/pocke/lemonade/releases/download/v1.1.1/lemonade_linux_amd64.tar.gz | tar -xz -C $(dirname $LEMONADE_PATH)
+    chmod 755 "$LEMONADE_PATH"
 fi
