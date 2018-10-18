2a8fff70f1684bc200f9e92b6234e5bbd02bb45e
Rohith Ravi <rohith.ravi@mixpanel.com>
2018-10-18 18:18:25 +0000

    brew: add jq to installed-by-default pkgs

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ in
     ;;
 esac
 
-brew install zsh ctags-exuberant global the_silver_searcher ruby neovim fzf tmux
+brew install zsh ctags-exuberant global the_silver_searcher ruby neovim fzf tmux jq
 
 # source code tagging
 # brew tap universal-ctags/universal-ctags
