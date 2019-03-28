dcdcbab6cd583aefa7b206358ec9f3674fa707d5
Rohith Ravi <rohith.ravi@mixpanel.com>
2019-03-28 22:59:55 +0000

    fzf: include symlinks since bazel creates symlinks everywhere

--- etc/zsh/custom/fzf.zsh
+++ etc/zsh/custom/fzf.zsh
@@ -3,5 +3,5 @@ export FZF_TMUX=0
 
 #export FZF_DEFAULT_COMMAND='ag -g ""'
 export FZF_DEFAULT_OPTS="--inline-info --exact"
-export FZF_DEFAULT_COMMAND='rg --files --no-follow'
+export FZF_DEFAULT_COMMAND='rg --files'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
