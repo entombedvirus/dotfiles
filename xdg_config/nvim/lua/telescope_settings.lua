f9c32c1141f497ff9b91c332316bf682da95211b
Rohith Ravi <rohith.ravi@mixpanel.com>
2021-06-09 01:26:21 +0000

    vim: switch <c-p> to use telescope with custom MRU logic

--- xdg_config/nvim/lua/telescope_settings.lua
+++ xdg_config/nvim/lua/telescope_settings.lua
@@ -1,4 +1,15 @@
+local action_set = require('telescope.actions.set')
+local action_state = require('telescope.actions.state')
 local actions = require('telescope.actions')
+local config = require('telescope.config')
+local finders = require('telescope.finders')
+local make_entry = require('telescope.make_entry')
+local path = require('telescope.path')
+local pickers = require('telescope.pickers')
+local themes = require('telescope.themes')
+local utils = require('telescope.utils')
+
+
 
 local telescope = require('telescope')
 telescope.setup{
@@ -68,6 +79,10 @@ keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>
 keymap("n", "<M-g>", "<cmd>lua require('telescope.builtin').grep_string()<cr>", opts)
 keymap("v", "<M-g>", "<esc><cmd>lua require('telescope_settings').grep_selection()<cr>", opts)
 
+-- ctrl-p style MRU
+keymap("n", "<leader>fp", "<cmd>FilesMru --tiebreak=end<cr>", opts)
+keymap("n", "<c-p>", "<cmd>lua require('telescope_settings').custom_mru()<cr>", opts)
+
 return {
     grep_selection = function()
         local _, srow, scol, _ = unpack(vim.fn.getpos("'<"))
@@ -77,7 +92,60 @@ return {
             return
         end
         local selection = vim.fn.getline("."):sub(scol, ecol)
-        print(selection)
         require('telescope.builtin').grep_string({search = selection})
-    end
+    end,
+
+    custom_mru = function()
+        -- TODO: stop relying on this bin file in another random plugin
+        local bin_file = os.getenv('HOME') ..'/.nvim/plugged/fzf-filemru/bin/filemru.sh'
+
+        local extract_fn_entry_maker = function()
+            local cwd = vim.fn.expand(opts.cwd or vim.fn.getcwd())
+            local prefix = "mru "
+
+            -- line sample: "mru go/src/mixpanel.com/discovery/discovery.go"
+            return function(line)
+                local filename = string.sub(line, #prefix + 1)
+                return {
+                    value = filename,
+                    ordinal = filename,
+                    filename = filename,
+                    display = function(_)
+                        local hl_group
+                        local display = path.make_relative(filename, cwd)
+                        display = string.sub(line, 1, #prefix) .. display
+                        display, hl_group = utils.transform_devicons(filename, display, false)
+
+                        if hl_group then
+                            return display, { { {1, 3}, hl_group } }
+                        else
+                            return display
+                        end
+                    end,
+                }
+            end
+        end
+
+        local picker = pickers.new({
+            results_title = 'Files',
+            -- Run an external command and show the results in the finder window
+            finder = finders.new_oneshot_job({bin_file, '--files'}, {
+                entry_maker = extract_fn_entry_maker(),
+            }),
+            sorter = config.values.file_sorter(),
+            attach_mappings = function()
+                 action_set.select:enhance({
+                     post = function()
+                         -- Will run after actions.select_default
+                         local entry = action_state.get_selected_entry()
+                         utils.get_os_command_output({bin_file, '--update', entry.filename})
+                     end,
+                 })
+                 return true
+            end,
+        }, themes.get_dropdown({
+            width = 0.3,
+        }))
+        picker:find()
+    end,
 }
