5af0bd819dae7a0dbe24d540238b8556247fcbfc
Rohith Ravi <rohith.ravi@mixpanel.com>
2021-12-28 21:28:14 +0000

    vim: refactor to clean up custom mru implementation

--- neovim/.config/nvim/lua/custom_mru.lua
+++ neovim/.config/nvim/lua/custom_mru.lua
@@ -1,39 +1,12 @@
-local Path = require('plenary.path')
 local utils = require('telescope.utils')
 local pickers = require('telescope.pickers')
 local config = require('telescope.config')
 local action_set = require('telescope.actions.set')
 local action_state = require('telescope.actions.state')
-local actions = require('telescope.actions')
 local themes = require('telescope.themes')
 local finders = require('telescope.finders')
 
 local max_mru_entries = 200
-
-local M = {}
-
-local mru_cache_file = function()
-    local cleaned_cwd = string.gsub(vim.fn.getcwd(), "/", "-");
-    local dir = vim.fn.stdpath('cache') .. '/custom-mru/proj' .. cleaned_cwd;
-    vim.fn.mkdir(dir, "p")
-    local cache_file = dir .. '/mru.txt'
-    if vim.fn.filereadable(cache_file) == 0 then
-        vim.fn.writefile({}, cache_file)
-    end
-    return cache_file
-end
-
-local find_command = function()
-    local mru_cmd = 'cat ' .. mru_cache_file()
-
-    local cwd = vim.fn.expand(vim.fn.getcwd())
-    local rg_cmd = 'rg --files'
-    if cwd == os.getenv('HOME') .. '/cl' then
-        rg_cmd = rg_cmd .. " --hidden --glob '!.git/' --glob '!git/'"
-    end
-    local dedupe_cmd = "awk '!seen[$0]++'"
-    return {'/bin/bash','-c', string.format('(%s; %s) | %s', mru_cmd, rg_cmd, dedupe_cmd)}
-end
 
 local make_mru_set = function(paths)
     local set = {}
@@ -53,14 +26,14 @@ local custom_entry_maker = function(path_list)
             filename = filename,
             display = function(_)
                 local hl_group
-                -- local display = Path:new(filename):make_relative(cwd)
                 local display = filename
                 if set[filename] then
+                    -- add a prefix to items from the mru list to tell them apart quickly
                     display = "mru " .. display
                 end
-                -- display = string.sub(line, 1, #prefix) .. display
+                -- prefix with a devicon if there is one available and add the
+                -- appropriate syntax highlight group to it
                 display, hl_group = utils.transform_devicons(filename, display, false)
-
                 if hl_group then
                     return display, { { {1, 3}, hl_group } }
                 else
@@ -71,14 +44,46 @@ local custom_entry_maker = function(path_list)
     end
 end
 
-local finder = function(path_list)
-    local cmd = find_command()
-    return finders.new_oneshot_job(cmd, {
-        entry_maker = custom_entry_maker(path_list),
+local MruPicker = {}
+-- `obj.foo` desugars to `getmetatable(obj).__index.foo`. So set
+-- MruPicker.__index to itself so that we end up doing getmetatable(obj).foo
+MruPicker.__index = MruPicker
+
+function MruPicker:cache_file()
+    -- memoize
+    if self.cache_file_path ~= nil then
+        return self.cache_file_path
+    end
+    local cleaned_cwd = string.gsub(self.cwd, "/", "-");
+    local dir = vim.fn.stdpath('cache') .. '/custom-mru/proj' .. cleaned_cwd;
+    vim.fn.mkdir(dir, "p")
+    self.cache_file_path = dir .. '/mru.txt'
+    if vim.fn.filereadable(self.cache_file_path) == 0 then
+        vim.fn.writefile({}, self.cache_file_path)
+    end
+    return self.cache_file_path
+end
+
+function MruPicker:find_command()
+    local mru_cmd = 'cat ' .. self:cache_file()
+    local rg_options = '--files'
+    if self.rg_options ~= nil then
+        rg_options = self.rg_options
+    end
+    -- put the output thru awk to dedupe them since entries in mru will appear
+    -- twice otherwise
+    local dedupe_cmd = "awk '!seen[$0]++'"
+    return {'/bin/bash','-c', string.format('(%s; rg %s) | %s', mru_cmd, rg_options, dedupe_cmd)}
+end
+
+function MruPicker:finder()
+    return finders.new_oneshot_job(self:find_command(), {
+        entry_maker = custom_entry_maker(self.path_list),
     })
 end
 
-local write_updated_mru_list = function(new_entry, path_list)
+function MruPicker:update_mru_list(new_entry)
+    local path_list = self.path_list
     for idx, path in ipairs(path_list) do
         if new_entry == path then
             table.remove(path_list, idx)
@@ -88,26 +93,23 @@ local write_updated_mru_list = function(new_entry, path_list)
     if #path_list > max_mru_entries then
         path_list = table.unpack(path_list, 1, max_mru_entries)
     end
-    vim.fn.writefile(path_list, mru_cache_file())
+    vim.fn.writefile(path_list, self:cache_file())
 end
 
-M.find = function()
-    local path_list = vim.fn.readfile(mru_cache_file(), '', max_mru_entries)
-    local picker = pickers.new({
+function MruPicker:new(o)
+    local obj = setmetatable(o or {}, self)
+    obj.cwd = obj.cwd or vim.fn.getcwd()
+    obj.path_list = vim.fn.readfile(obj:cache_file(), '', max_mru_entries)
+    obj.picker = pickers.new({
         results_title = 'Files',
-        -- Run an external command and show the results in the finder window
-        -- finder = finders.new_oneshot_job({bin_file, '--files'}, {
-        --     entry_maker = extract_fn_entry_maker(),
-        -- }),
-        finder = finder(path_list),
+        finder = obj:finder(),
         sorter = config.values.file_sorter(),
         attach_mappings = function()
             action_set.select:enhance({
                 post = function()
                     -- Will run after actions.select_default
                     local entry = action_state.get_selected_entry()
-                    -- utils.get_os_command_output({bin_file, '--update', entry.filename})
-                    write_updated_mru_list(entry.filename, path_list)
+                    obj:update_mru_list(entry.filename)
                 end,
             })
             return true
@@ -117,7 +119,26 @@ M.find = function()
             width = 0.6,
         },
     }))
-    picker:find()
+    return obj
+end
+
+local M = {}
+
+M.find = function()
+    local cwd = vim.fn.getcwd()
+
+    local rg_options
+    if cwd == os.getenv('HOME') .. '/cl' then
+        -- in the dotfiles repo, we need to see hidden files, but not inside
+        -- the .git directory
+        rg_options = "--files --hidden --glob '!.git/' --glob '!git/'"
+    end
+
+    local p = MruPicker:new {
+        cwd = cwd,
+        rg_options = rg_options,
+    }
+    p.picker:find()
 end
 
 return M
