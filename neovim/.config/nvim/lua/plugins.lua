30f7fe12e0f6c6698f4a6caa34b8b0b25510ba90
Rohith Ravi <rohith.ravi@mixpanel.com>
2022-01-01 22:06:00 +0000

    vim: add lsp_progress to lualine status

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -76,7 +76,10 @@ return require('packer').startup(function(use)
 
   use {
       'nvim-lualine/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true},
+      requires = {
+        {'kyazdani42/nvim-web-devicons', opt = true},
+        {'arkav/lualine-lsp-progress'},
+      },
       config = function()
           require('lualine').setup {
               options = {
@@ -86,7 +89,7 @@ return require('packer').startup(function(use)
               sections = {
                  lualine_a = {'mode'},
                  lualine_b = {'branch'},
-                 lualine_c = {{'filename', path = 1}},
+                 lualine_c = {{'filename', path = 1}, {'lsp_progress'}},
                  lualine_x = {'encoding', 'fileformat', 'filetype'},
                  lualine_y = {'progress'},
                  lualine_z = {'location'},
