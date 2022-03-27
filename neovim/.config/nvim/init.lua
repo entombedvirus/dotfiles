b1504ec2e3ebbfc130002b69fb38f5d7fceb0903
Rohith Ravi <rohith.ravi@mixpanel.com>
2022-03-27 04:01:40 +0000

    vim: cargo culting some of tj's snippets

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,8 @@
 require('plugins')
+require('roh.globals')
+
+-- Turn off builtin plugins I do not use.
+require "roh.disable_builtin"
 
 vim.cmd('runtime lua/settings/editor.vim')
 require('settings.colorscheme')
--- neovim/.config/nvim/lua/settings/luasnip.lua
+++ neovim/.config/nvim/lua/settings/luasnip.lua
@@ -9,7 +9,7 @@ local types = require "luasnip.util.types"
 ls.config.set_config {
   -- This tells LuaSnip to remember to keep around the last snippet.
   -- You can jump back into it even if you move outside of the selection
-  history = false,
+  history = true,
 
   -- This one is cool cause if you have dynamic snippets, it updates as you type!
   updateevents = "TextChanged,TextChangedI",
@@ -53,9 +53,51 @@ local snippet = ls.s
 --  Multiple lines are by passing a table of strings.
 --  t { "line 1", "line 2" }
 local t = ls.text_node
-ls.snippets = {
-    all = {
-        snippet('foo', t'bar'),
-        ls.parser.parse_snippet({trig = "lsp"}, "$1 is ${2|hard,easy,challenging|}"),
-    },
+
+-- Insert Node
+--  Creates a location for the cursor to jump to.
+--      Possible options to jump to are 1 - N
+--      If you use 0, that's the final place to jump to.
+--
+--  To create placeholder text, pass it as the second argument
+--      i(2, "this is placeholder text")
+local i = ls.insert_node
+
+local shortcut = function(val)
+  if type(val) == "string" then
+    return { t { val }, i(0) }
+  end
+
+  if type(val) == "table" then
+    for k, v in ipairs(val) do
+      if type(v) == "string" then
+        val[k] = t { v }
+      end
+    end
+  end
+
+  return val
+end
+
+local make = function(tbl)
+  local result = {}
+  for k, v in pairs(tbl) do
+    table.insert(result, (snippet({ trig = k, desc = v.desc }, shortcut(v))))
+  end
+
+  return result
+end
+
+local snippets = {}
+
+snippets.all = {
+    snippet('foo', t'bar'),
+    ls.parser.parse_snippet({trig = "lsp"}, "$1 is ${2|hard,easy,challenging|}"),
 }
+
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/roh/snips/ft/*.lua", true)) do
+  local ft = vim.fn.fnamemodify(ft_path, ":t:r")
+  snippets[ft] = make(loadfile(ft_path)())
+end
+
+ls.snippets = snippets
