dae7b32ee7d8554788513899494d6cbecef1fb2d
Rohith Ravi <rohith.ravi@mixpanel.com>
2022-06-20 20:38:27 +0000

    vim: better looking folds

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -563,6 +563,47 @@ packer.startup(function(use)
 		},
 
 		use 'digitaltoad/vim-pug',
+
+		-- better looking folds
+		use {
+			'kevinhwang91/nvim-ufo',
+			requires = 'kevinhwang91/promise-async',
+			config = function()
+				local handler = function(virtText, lnum, endLnum, width, truncate)
+					local newVirtText = {}
+					local suffix = ('  %d '):format(endLnum - lnum)
+					local sufWidth = vim.fn.strdisplaywidth(suffix)
+					local targetWidth = width - sufWidth
+					local curWidth = 0
+					for _, chunk in ipairs(virtText) do
+						local chunkText = chunk[1]
+						local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						if targetWidth > curWidth + chunkWidth then
+							table.insert(newVirtText, chunk)
+						else
+							chunkText = truncate(chunkText, targetWidth - curWidth)
+							local hlGroup = chunk[2]
+							table.insert(newVirtText, { chunkText, hlGroup })
+							chunkWidth = vim.fn.strdisplaywidth(chunkText)
+							-- str width returned from truncate() may less than 2nd argument, need padding
+							if curWidth + chunkWidth < targetWidth then
+								suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+							end
+							break
+						end
+						curWidth = curWidth + chunkWidth
+					end
+					table.insert(newVirtText, { suffix, 'MoreMsg' })
+					return newVirtText
+				end
+				vim.wo.foldcolumn = '0'
+				vim.wo.foldlevel = 99 -- feel free to decrease the value
+				vim.wo.foldenable = true
+				require('ufo').setup {
+					fold_virt_text_handler = handler,
+				}
+			end,
+		}
 	}
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
