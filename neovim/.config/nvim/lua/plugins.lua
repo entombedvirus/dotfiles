2a70395a11c5d1f24d41d12bc96e0954e98af91f
Rohith Ravi <rohith.ravi@mixpanel.com>
2022-09-30 17:12:27 +0000

    vim: rust dap config

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -559,8 +559,50 @@ packer.startup(function(use)
 					prompt_user_for_args = true,
 				},
 			}
+
+			-- Rust / C / C++
+			dap.adapters.lldb = {
+				type = 'executable',
+				command = 'lldb-vscode', -- adjust as needed, must be absolute path
+				name = 'lldb'
+			}
+			dap.configurations.cpp = {
+				{
+					name = 'Launch',
+					type = 'lldb',
+					request = 'launch',
+					program = function()
+						return vim.fn.input('Path to executable: ', last_user_input or vim.fn.getcwd() .. '/', 'file')
+					end,
+					cwd = '${workspaceFolder}',
+					stopOnEntry = false,
+					args = {},
+
+					-- ??
+					-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+					--
+					--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+					--
+					-- Otherwise you might get the following error:
+					--
+					--    Error on launch: Failed to attach to the target process
+					--
+					-- But you should be aware of the implications:
+					-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+					-- runInTerminal = false,
+				},
+			}
+
+			-- If you want to use this for Rust and C, add something like this:
+			dap.configurations.c = dap.configurations.cpp
+			dap.configurations.rust = dap.configurations.cpp
+
 			local opts = { silent = true, noremap = true }
 			vim.api.nvim_set_keymap('n', '<leader>dd', "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
+			vim.api.nvim_set_keymap('n', '<leader>dB',
+				"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>", opts)
+			vim.api.nvim_set_keymap('n', '<leader>ddl',
+				"<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>", opts)
 			vim.api.nvim_set_keymap('n', '<leader>dc', "<cmd>lua require'dap'.continue()<cr>", opts)
 			vim.api.nvim_set_keymap('n', '<leader>dl', "<cmd>lua require'dap'.run_last()<cr>", opts)
 			vim.api.nvim_set_keymap('n', '<leader>dn', "<cmd>lua require'dap'.step_over()<cr>", opts)
