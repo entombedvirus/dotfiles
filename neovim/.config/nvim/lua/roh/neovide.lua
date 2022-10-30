3075081b7024fbf7f1d66254e74d94b4e50f7c07
Rohith Ravi <rohith.ravi@mixpanel.com>
2022-10-30 18:41:36 +0000

    vim: neovide tweaks

--- neovim/.config/nvim/lua/roh/neovide.lua
+++ neovim/.config/nvim/lua/roh/neovide.lua
@@ -1,15 +1,17 @@
 -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
 vim.g.neovide_transparency = 0.0
-vim.g.transparency = 0.9
+vim.g.transparency = 0.95
 vim.g.neovide_background_color = '#0f1117' .. vim.fn.printf('%x', vim.fn.float2nr(255 * vim.g.transparency))
 
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 
 vim.g.neovide_input_macos_alt_is_meta = true
+vim.g.neovide_fullscreen = true
+
 
 -- See https://github.com/neovide/neovide/blob/main/website/docs/configuration.md#display
-vim.opt.guifont = { "Iosevka SS08 Light:h18:#e-subpixelantialias:#h-none" }
+vim.opt.guifont = { "Iosevka SS08 Light:h18:#e-subpixelantialias:#h-full" }
 
 -- dynamically change scale
 local function changeScaleFactor(delta)
