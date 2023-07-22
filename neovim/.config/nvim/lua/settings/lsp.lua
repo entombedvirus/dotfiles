11f2eee9bd0810434c39d9831c180c20e99948ab
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-07-22 16:40:48 +0000

    neovim: lsp: lua: make popups go away
    See: https://github.com/neovim/nvim-lspconfig/issues/1700#issuecomment-1033127328

--- neovim/.config/nvim/lua/settings/lsp.lua
+++ neovim/.config/nvim/lua/settings/lsp.lua
@@ -291,7 +291,8 @@ local function get_lsp_opts(lang)
 				},
 				workspace = {
 					-- Make the server aware of Neovim runtime files
-					-- library = vim.api.nvim_get_runtime_file('', true),
+					library = vim.api.nvim_get_runtime_file('', true),
+					checkThirdParty = false,
 				},
 				-- Do not send telemetry data containing a randomized but unique identifier
 				telemetry = {
