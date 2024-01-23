71c9dd4193a1e7d4ca1c128aaffbc55ffdc7a352
Rohith Ravi <rohith.ravi@mixpanel.com>
2024-01-23 10:48:11 -0800

    vim: ts-context: only show 5 lines of context

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -109,7 +109,9 @@ return {
 		'nvim-treesitter/nvim-treesitter-context',
 		dependencies = { 'nvim-treesitter/nvim-treesitter' },
 		config = function()
-			require 'treesitter-context'.setup {}
+			require 'treesitter-context'.setup {
+				max_lines = 5,
+			}
 		end,
 	},
 
