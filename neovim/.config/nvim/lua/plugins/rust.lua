e0239934ed67008e1ee20c269ee96c18f9b813e8
Rohith Ravi <rohith.ravi@mixpanel.com>
2024-05-27 11:55:41 -0700

    nvim: rust: refactor keyboard shortcuts

--- neovim/.config/nvim/lua/plugins/rust.lua
+++ neovim/.config/nvim/lua/plugins/rust.lua
@@ -26,4 +26,68 @@ return {
 			}
 		}
 	end,
+	keys = {
+		{
+			"<leader>rd",
+			function() vim.cmd.RustLsp('debuggables') end,
+			ft = "rust",
+			silent = true,
+			desc = "list and run debuggable targets"
+		},
+		{
+			"<leader><F5>",
+			function() vim.cmd.RustLsp({ 'runnables' }) end,
+			ft = "rust",
+			silent = true,
+			desc = "list and run runnable targets"
+		},
+		{
+			"<space><F5>",
+			function() vim.cmd.RustLsp({ 'run' }) end,
+			ft = "rust",
+			silent = true,
+			desc = "run target at cursor"
+		},
+		{
+			"<F5>",
+			function() vim.cmd.RustLsp({ 'run', bang = true }) end,
+			ft = "rust",
+			silent = true,
+			desc = "run last target"
+		},
+		{
+			"<leader><F4>",
+			function() vim.cmd.RustLsp({ 'debuggables' }) end,
+			silent = true,
+			desc = "list debuggable targets"
+		},
+		{
+			"<space><F4>",
+			function()
+				vim.cmd.RustLsp({ 'debug' })
+			end,
+			silent = true,
+			desc = 'debug target at cursor'
+		},
+		{
+			"<F4>",
+			function() vim.cmd.RustLsp({ 'debug', bang = true }) end,
+			silent = true,
+			desc = 'debug last target'
+		},
+		{
+			"<space>ha",
+			function() vim.cmd.RustLsp({ 'hover', 'actions' }) end,
+			silent = true,
+			desc = 'show hover actions at cursor'
+		},
+		{
+			"<space>ca",
+			function()
+				vim.cmd.RustLsp({ 'codeAction' })
+			end,
+			silent = true,
+			desc = 'show code actions at cursor'
+		}
+	},
 }
