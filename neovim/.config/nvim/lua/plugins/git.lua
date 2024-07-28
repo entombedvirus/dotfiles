4945a9e0a6e5a9496591b2d71c833b5963dcb09e
Rohith Ravi <entombedvirus@gmail.com>
2024-07-28 07:57:08 -0700

    nvim: gitsigns: remove deprecated HL groups

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -3,11 +3,11 @@ return {
 		'lewis6991/gitsigns.nvim',
 		opts = {
 			signs = {
-				add          = { hl = 'GitSignsAdd', text = '+', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-				change       = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-				delete       = { hl = 'GitSignsDelete', text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-				topdelete    = { hl = 'GitSignsDelete', text = '?', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-				changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+				add          = { text = '+' },
+				change       = { text = '~' },
+				delete       = { text = '-' },
+				topdelete    = { text = '?' },
+				changedelete = { text = '~' },
 			},
 			numhl = false,
 			linehl = false,
