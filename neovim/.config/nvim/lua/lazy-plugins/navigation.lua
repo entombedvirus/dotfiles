cccf27b41205fbd362d02af75dbc528d620a2457
Rohith Ravi <entombedvirus@gmail.com>
2025-02-19 17:12:05 -0800

    nvim: make telescope work from dirvish

--- neovim/.config/nvim/lua/lazy-plugins/navigation.lua
+++ neovim/.config/nvim/lua/lazy-plugins/navigation.lua
@@ -1,4 +1,17 @@
 return {
-	'justinmk/vim-dirvish',
+	{
+		'justinmk/vim-dirvish',
+		config = function()
+			-- remove the <c-p> mapping since it interferes with telescope
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "dirvish",
+				callback = function()
+					-- use pcall to suppress warnings about no such mapping
+					pcall(vim.keymap.del, 'n', '<C-p>', { buffer = true })
+				end,
+				group = vim.api.nvim_create_augroup("dirvish_config", { clear = true })
+			})
+		end,
+	},
 	'rbtnn/vim-jumptoline',
 }
