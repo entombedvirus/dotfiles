0dc33e8fe2c9df4f9d51b5d2ae7a78781ef752e7
Rohith Ravi <entombedvirus@gmail.com>
2025-07-06 10:27:45 -0700

    nvim: typescript-tools: disable duplicate diagnostics

--- neovim/.config/nvim/lua/lazy-plugins/typescript-tools.lua
+++ neovim/.config/nvim/lua/lazy-plugins/typescript-tools.lua
@@ -2,7 +2,26 @@ return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	config = function()
+		-- ensure that the lsp settings is loaded so that we can re-use it
+		require("roh/lsp")
 		local opts = vim.lsp.config["*"]
-		require("typescript-tools").setup(opts)
+		assert(not vim.tbl_isempty(opts), "LSP settings not found for typescript-tools.nvim")
+
+		local base_on_attach = opts.on_attach
+		require("typescript-tools").setup(vim.tbl_deep_extend('force', opts, {
+			on_attach = function(client, bufnr)
+				if (base_on_attach ~= nil) then
+					base_on_attach(client, bufnr)
+				end
+				-- prevent duplicate diagnostic from both eslint and typescript-tools by
+				-- disabling one of them. eslint diagnostics seems like a super set, so
+				-- disabling typescript-tools' diagnostics
+				local ns_id = vim.lsp.diagnostic.get_namespace(client.id)
+				vim.diagnostic.enable(false, {
+					ns_id = ns_id,
+					bufnr = bufnr,
+				})
+			end
+		}))
 	end,
 }
