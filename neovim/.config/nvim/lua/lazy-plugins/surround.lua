f1972b47ca9eed4db170948f2f35534bca84cfeb
Rohith Ravi <entombedvirus@gmail.com>
2026-02-24 10:33:43 -0800

    nvim: fixes post upgrade

--- neovim/.config/nvim/lua/lazy-plugins/surround.lua
+++ neovim/.config/nvim/lua/lazy-plugins/surround.lua
@@ -1,6 +1,9 @@
 return {
 	"kylechui/nvim-surround",
 	version = "*", -- Use for stability; omit to  `main` branch for the latest features
+	init = function()
+		vim.g.nvim_surround_no_mappings = true
+	end,
 	config = function()
 		require("nvim-surround").setup({
 			-- Configuration here, or leave empty to  defaults
@@ -13,19 +16,39 @@ return {
 					add = { "|", "|" },
 				}
 			},
-			keymaps = {
-				insert = "<C-s>",
-				insert_line = "<C-s><C-s>",
-				normal = "ys",
-				normal_cur = "yss",
-				normal_line = "yS",
-				normal_cur_line = "ySS",
-				visual = "S",
-				visual_line = "gS",
-				delete = "ds",
-				change = "cs",
-			},
 		})
+
+		vim.keymap.set("i", "<C-s>", "<Plug>(nvim-surround-insert)", {
+			desc = "Add a surrounding pair around the cursor (insert mode)",
+		})
+		vim.keymap.set("i", "<C-s><C-s>", "<Plug>(nvim-surround-insert-line)", {
+			desc = "Add a surrounding pair around the cursor, on new lines (insert mode)",
+		})
+		vim.keymap.set("n", "ys", "<Plug>(nvim-surround-normal)", {
+			desc = "Add a surrounding pair around a motion (normal mode)",
+		})
+		vim.keymap.set("n", "yss", "<Plug>(nvim-surround-normal-cur)", {
+			desc = "Add a surrounding pair around the current line (normal mode)",
+		})
+		vim.keymap.set("n", "yS", "<Plug>(nvim-surround-normal-line)", {
+			desc = "Add a surrounding pair around a motion, on new lines (normal mode)",
+		})
+		vim.keymap.set("n", "ySS", "<Plug>(nvim-surround-normal-cur-line)", {
+			desc = "Add a surrounding pair around the current line, on new lines (normal mode)",
+		})
+		vim.keymap.set("x", "S", "<Plug>(nvim-surround-visual)", {
+			desc = "Add a surrounding pair around a visual selection",
+		})
+		vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)", {
+			desc = "Add a surrounding pair around a visual selection, on new lines",
+		})
+		vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)", {
+			desc = "Delete a surrounding pair",
+		})
+		vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)", {
+			desc = "Change a surrounding pair",
+		})
+
 		-- emulate a shortcut from vim-surround
 		vim.keymap.set("i", "<C-s><C-s><C-]>", "<C-s><C-s>}", { remap = true, silent = true })
 	end
